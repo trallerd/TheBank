09b840d2722ea8305711865eafc2ff82b0246d24
Trallerd <jessicacristec@gmail.com>
2020-11-24 18:11:40 -0300

    Start graph and layout

---
+++ app/src/main/java/com/trallerd/thebank/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.trallerd.thebank
+
+import com.trallerd.thebank.R
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
